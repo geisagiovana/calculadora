7657a48b14fb1684f99c02091ec817424f746f5f
geisagiovana <geisa.giovana@yahoo.com.br>
2016-07-19 11:12:09 -0300

    ok

---
+++ ProgramOp/Operacao.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOp
+{
+    class Operacao
+    {
+        const double pi = 3.14159;
+
+        private Char sinal;
+
+        private Decimal valor1;
+
+        private Decimal valor2;
+
+        private Decimal resultado;
+
+        public Operacao(Char sinal, Decimal valor1, Decimal valor2)
+        {
+            this.sinal = sinal;
+            this.valor1 = valor1;
+            this.valor2 = valor2;
+
+            switch (sinal)
+            {
+                case '+':
+                    {
+                        somar();
+                        break;
+                    }
+                case 'c':
+                    {
+                        converter();
+                        break;
+                    }
+            }
+        }
+
+        public Decimal somar()
+        {
+           return this.resultado = valor1 + valor2;
+        }
+
+        //Converte o valor r para o valor da area do circulo
+        public void converter()
+        {
+            this.resultado = Convert.ToDecimal(pi * Convert.ToDouble(valor1) * Convert.ToDouble(valor1));
+        }
+
+        public Char getSinal()
+        {
+            return sinal;
+        }
+
+        public void setSinal(Char sinal)
+        {
+            this.sinal = sinal;
+        }
+
+        public Decimal getValor1()
+        {
+            return valor1;
+        }
+
+        public void setValor1(Decimal valor1)
+        {
+            this.valor1 = valor1;
+        }
+
+        public Decimal getValor2()
+        {
+            return valor2;
+        }
+
+        public void setValor2(Decimal valor2)
+        {
+            this.valor2 = valor2;
+        }
+
+        public Decimal getResultado()
+        {
+            return resultado;
+        }
+
+        public void setResultado(Decimal resultado)
+        {
+            this.resultado = resultado;
+        }
+
+    }
+}
---
+++ ProgramOp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1 Hello World 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            *//*2 Somar
+            Operacao operacao = new Operacao();
+            int valor = operacao.somar(2, 2);
+            Console.WriteLine("Resultado:"+ valor);
+            // Keep the console window open in debug mode.
+             Console.ReadKey();
+            */
+            /* 3 Calcular area circulo
+            Operacao operacao = new Operacao();
+            Console.WriteLine("Enter Radius: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double areaCirculo = operacao.converter(r);
+            Console.WriteLine("Radius: " + r + " Area: " + areaCirculo);
+            Console.ReadKey();
+
+            */
+            Operacao operacao;
+
+            Console.WriteLine("Enter valor1: ");
+            Decimal valor1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter sinal: ");
+            Char sinal = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter valor2: ");
+            Decimal valor2 = Convert.ToDecimal(Console.ReadLine());
+
+            operacao = new Operacao(sinal, valor1, valor2);
+
+            Console.WriteLine("Resultado: 5 {0} ", operacao.getResultado());
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
